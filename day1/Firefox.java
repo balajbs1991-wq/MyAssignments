455422874e58a12869bc4240dba9c258dd29a03f
balajbs1991-wq <balajbs1991@gmail.com>
2026-04-26 05:27:06 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,12 @@
+package main.java.org.week1.day1;
+public class Firefox {
+    public static void main(String[] args) {
+        float browserVersion = 100.2f;
+        String browser = "Firefox";
+        boolean isVisible = true;
+        int releaseYear = 2004;
+        char browserLogo = 'f';
+
+        System.out.println(browserVersion + " " + browser + " " + isVisible + " " + releaseYear + " " + browserLogo);
+    }
+}
